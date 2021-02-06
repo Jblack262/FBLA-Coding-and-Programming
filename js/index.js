d971739758c1f1cc52918feb2607bc7fd36415a7
Jblack262 <71471784+Jblack262@users.noreply.github.com>
2021-02-06 09:01:21 -0700

    final commit

--- js/index.js
+++ js/index.js
@@ -634,5 +634,5 @@ function finalSubmit() {
         alert("You answered " + right + " questions correctly. " + score + "%");
     }
 
-    window.location.href = "main.html";
+    window.location.href = "index.html";
 }
